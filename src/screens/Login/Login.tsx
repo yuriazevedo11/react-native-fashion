ac403dcd5ff065d92467a5678ad7769e1de173e1
Yuri Azevedo <yuriazevedo11@gmail.com>
2020-07-25 15:24:28 -0300

    ✨ feat: Finish footer on Login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Box, Container, SocialLogin } from '@/components';
+import { Box, Container, SocialLogin, Button, Text } from '@/components';
 import { AuthenticationProps } from '@/routes/types';
 
 // import { Container } from './Login.styles';
 
 const Login: React.FC<AuthenticationProps<'Login'>> = () => {
+  const footer = (
+    <>
+      <SocialLogin />
+      <Box alignItems="center">
+        <Button variant="transparent" onPress={() => {}}>
+          <Box flexDirection="row" justifyContent="center">
+            <Text color="white">Don&apos;t have have an account?</Text>
+            <Text color="primary" marginLeft="s">
+              Sign Up Here
+            </Text>
+          </Box>
+        </Button>
+      </Box>
+    </>
+  );
+
   return (
-    <Container footer={<SocialLogin />}>
+    <Container {...{ footer }}>
       <Box flex={1} />
     </Container>
   );
