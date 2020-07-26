d7b05befd8dd5d2f42562ffe9a9e268496fea547
Yuri Azevedo <yuriazevedo11@gmail.com>
2020-07-26 20:39:21 -0300

    ♻️ refactor: Use forwardRef on TextField component

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { TextInputProps, TextInput, StyleSheet } from 'react-native';
 import { Feather as Icon } from '@expo/vector-icons';
 
@@ -12,57 +12,55 @@ interface TextFieldProps extends TextInputProps {
   error?: string;
 }
 
-const TextField = ({
-  icon,
-  touched,
-  error,
-  ...textInputProps
-}: TextFieldProps) => {
-  const theme = useTheme();
-  const validationSize = theme.borderRadii.m * 2;
-  const restyleColor = (() => {
-    if (touched && error) return 'danger';
-    if (touched && !error) return 'primary';
-    return 'baseText';
-  })();
-  const color = theme.colors[restyleColor];
+const TextField = forwardRef<TextInput, TextFieldProps>(
+  ({ icon, touched, error, ...textInputProps }, ref) => {
+    const theme = useTheme();
+    const validationSize = theme.borderRadii.m * 2;
+    const restyleColor = (() => {
+      if (touched && error) return 'danger';
+      if (touched && !error) return 'primary';
+      return 'baseText';
+    })();
+    const color = theme.colors[restyleColor];
 
-  return (
-    <Box
-      height={48}
-      padding="s"
-      flexDirection="row"
-      borderRadius="s"
-      alignItems="center"
-      borderColor={restyleColor}
-      borderWidth={StyleSheet.hairlineWidth}
-    >
-      <Box padding="s">
-        <Icon name={icon} size={16} {...{ color }} />
-      </Box>
-
-      <Box flex={1}>
-        <TextInput
-          placeholderTextColor={color}
-          underlineColorAndroid="transparent"
-          {...textInputProps}
-        />
-      </Box>
-
-      {touched && (
-        <Box
-          height={validationSize}
-          width={validationSize}
-          borderRadius="m"
-          justifyContent="center"
-          alignItems="center"
-          backgroundColor={error ? 'danger' : 'primary'}
-        >
-          <Icon name={error ? 'x' : 'check'} color="white" size={16} />
+    return (
+      <Box
+        height={48}
+        padding="s"
+        flexDirection="row"
+        borderRadius="s"
+        alignItems="center"
+        borderColor={restyleColor}
+        borderWidth={StyleSheet.hairlineWidth}
+      >
+        <Box padding="s">
+          <Icon name={icon} size={16} {...{ color }} />
         </Box>
-      )}
-    </Box>
-  );
-};
+
+        <Box flex={1}>
+          <TextInput
+            placeholderTextColor={color}
+            underlineColorAndroid="transparent"
+            {...{ ref }}
+            {...textInputProps}
+          />
+        </Box>
+
+        {touched && (
+          <Box
+            height={validationSize}
+            width={validationSize}
+            borderRadius="m"
+            justifyContent="center"
+            alignItems="center"
+            backgroundColor={error ? 'danger' : 'primary'}
+          >
+            <Icon name={error ? 'x' : 'check'} color="white" size={16} />
+          </Box>
+        )}
+      </Box>
+    );
+  },
+);
 
 export default TextField;
